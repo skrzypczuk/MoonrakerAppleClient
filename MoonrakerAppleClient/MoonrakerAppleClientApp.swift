b153a237b0cdac6f25eb5896c6e8ee3a0cb19b20
skrzypczuk <skrzypczakmiki@gmail.com>
2025-11-22 19:47:43 +0100

    add url for docker image for virtual printer

--- MoonrakerAppleClient/MoonrakerAppleClientApp.swift
+++ MoonrakerAppleClient/MoonrakerAppleClientApp.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct PreviewEnvironment {
-    static let printer: Printer = Printer(url: URL(string:"ws://192.168.88.39:7125/websocket")!)
+    static let printer: Printer = Printer(url: URL(string: "ws://localhost:7125/websocket")!)
 }
 
 @main
 struct MoonrakerAppleClientApp: App {
     //TODO: add array of printers, change "printer" to "selectedPrinter"
-    @StateObject private var printer = Printer(url: URL(string:"ws://192.168.88.39:7125/websocket")!) //change url to your printer websocket
+    @StateObject private var printer = Printer(url: URL(string: "ws://localhost:7125/websocket")!)  //change url to your printer websocket
     var body: some Scene {
         WindowGroup {
             ContentView()
